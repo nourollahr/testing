b33ccf94ce0fbdc7db366930a005a6e8bab953eb
nourollah rezaei <nourollahr@gmail.com>
2021-12-01 01:05:56 +0330

    test view
    http test: create test for  layout view rendering with login type user and admin

---
+++ tests/Feature/Views/LayoutViewTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Views;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LayoutViewTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLayoutViewRenderedWhenUserIsAdmin()
+    {
+        $user = User::factory()->state(['type' => 'admin'])->create();
+
+        $this->actingAs($user);
+
+        $view = $this->view('layouts.layout');
+
+        $view->assertSee('<a href="/admin/dashboard">admin panel</a>', false);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLayoutViewRenderedWhenUserIsNotAdmin()
+    {
+        $user = User::factory()->state(['type' => 'user'])->create();
+
+        $this->actingAs($user);
+
+        $view = $this->view('layouts.layout');
+
+        $view->assertDontSee('<a href="/admin/dashboard">admin panel</a>', false);
+    }
+}
